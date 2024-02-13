7d18032c460a99fbf5b948c16af678b62a0da7c3
miteshchakma@gmail.com <miteshchakma@gmail.com>
2024-02-13 16:16:33 +0200

    Added Experience section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     new Typed('#typed-text', {
-        strings: ["Python Developer @ Turing", "Certified Data Scientist"],
-        typeSpeed: 80,
+        strings: ["Python Developer @ Turing", "Data Scientist"],
+        typeSpeed: 110,
         backSpeed: 50,
         loop: true,
     });
