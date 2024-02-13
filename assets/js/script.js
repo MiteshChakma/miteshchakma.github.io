a0e4c364016f50842854a019e6afd1e7962f7e64
miteshchakma@gmail.com <miteshchakma@gmail.com>
2024-02-13 15:46:58 +0200

    welsome page and the project structure

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    new Typed('#typed-text', {
+        strings: ["Python Developer @ Turing", "Certified Data Scientist"],
+        typeSpeed: 80,
+        backSpeed: 50,
+        loop: true,
+    });
+});
